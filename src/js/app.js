58bd7a64d3bec474cf1a9683017596be23b9932a
Jane Marimo <jenny.bulatova@gmail.com>
2024-02-17 12:23:41 +0300

    eslintrc

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
-import { Validator } from './Validator';
+import { Validator } from './Validator'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const validator = new Validator();
+    const validator = new Validator()
 
-  document.querySelector('.input').addEventListener('input', validator.handleCardInput.bind(validator));
-  document.querySelector('.button').addEventListener('click', validator.validateCard.bind(validator));
-});
+    document
+        .querySelector('.input')
+        .addEventListener('input', validator.handleCardInput.bind(validator))
+    document
+        .querySelector('.button')
+        .addEventListener('click', validator.validateCard.bind(validator))
+})
